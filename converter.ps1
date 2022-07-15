e9cb49e8e74dc38e12826fae5e7dc9d602969fc0
Paulo Giovanny <pgs.magno@gmail.com>
2022-07-15 03:04:23 -0300

    adds files (powershell and EXE)

---
+++ converter.ps1
@@ -0,0 +1,48 @@
+﻿echo ""
+echo ""
+echo "                                   ::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "                                   ::            .--.                                ::
+                                   :: ╦ ╦╔╦╗╔═╗ { {} }  ╔═╗╔═╗╔╗╔╦  ╦╔═╗╦═╗╔╦╗╔═╗╦═╗ ::
+                                   :: ║ ║ ║ ╠╣   }  {   ║  ║ ║║║║╚╗╔╝║╣ ╠╦╝ ║ ║╣ ╠╦╝ ::
+                                   :: ╚═╝ ╩ ╚   { {} }  ╚═╝╚═╝╝╚╝ ╚╝ ╚═╝╩╚═ ╩ ╚═╝╩╚═ ::
+                                   ::            '--'                                ::"
+echo "                                   ::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo ""
+
+$firstReply = ""
+while ($firstReply -ne "YES"){
+    $firstReply = Read-Host "`nDo you want to convert all 'CSV' and 'TXT' files in this folder to 'UFT-8 with BOM'?`nType [YES] to confirm or [NO] to Exit"
+    
+    if($firstReply -eq "NO") {        
+        Write-Host "`nConversion aborted! Press [ENTER] to exit..."
+        Read-Host
+        exit 
+    } 
+    if($firstReply -ne "YES") {
+        echo "`nCommand not recognized."
+    }
+}
+
+$secondReply = Read-Host "`nWARNING! This action cannot be undone. Are you sure you want to continue? Type [YES] to confirm"
+
+if ($firstReply -eq "YES" -and $secondReply -eq "YES") {
+
+    echo ""
+    foreach($i in ls "*.*") {
+
+        if($i.Extension -ieq  ".txt" -or $i.Extension -ieq ".csv"){
+
+        Write-Host "Converting" $i.Name "...OK!"
+        $temp = Get-Content $i.fullname
+        Out-File -filepath $i.fullname -inputobject $temp -encoding utf8 -force
+        }
+    }
+    
+    Write-Host "`nPress [ENTER] to exit..."
+    Read-Host
+    exit
+} else{
+    Write-Host "`nConversion aborted! Press [ENTER] to exit..."
+    Read-Host
+    exit
+}
